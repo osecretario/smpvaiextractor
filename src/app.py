f01d2feb5d363e1a637ff6ffb8a53c34294b985f
Willian K. Sanada <41298785+wsanada@users.noreply.github.com>
2025-05-05 12:44:04 -0300

    Update app.py "*"

--- src/app.py
+++ src/app.py
@@ -25,7 +25,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['null'],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
